208cd1bda5741b9186028b19115a9fe9f34e8ccd
Nishanth <krishna.churchmal00@gmail.com>
2024-08-31 16:50:47 -0700

    removed mads/ to go to home

--- countdown/urls.py
+++ countdown/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mads/', include("mads.urls")),
+    path('', include("mads.urls")),
+    
+    # HAD TO USE THIS AS WHEN DEBUG=Flase static files like css and js were not loadind
+    # https://stackoverflow.com/questions/5836674/why-does-debug-false-setting-make-my-django-static-files-access-fail
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
